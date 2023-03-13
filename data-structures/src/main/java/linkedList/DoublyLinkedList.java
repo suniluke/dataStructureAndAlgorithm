dcfd0f79f6a3b9b8b7938bf051af3b609dddf099
Suni <sunil.uke@walmart.com>
2023-03-13 09:17:56 +0530

    Doubly LinedList implementation

---
+++ data-structures/src/main/java/linkedList/DoublyLinkedList.java
@@ -0,0 +1,226 @@
+package linkedList;
+
+import java.util.Objects;
+
+public class DoublyLinkedList {
+
+    //What features it should have
+    /*
+        * inertBefore
+        * insertAfter
+        * insertAtPosition
+        * removeGivenNode
+        * RemoveNodesWithGivenValue
+        * setHead
+        * setTail
+        * containsNodeWithValue
+     */
+
+
+    public class Node{
+        Integer value;
+        Node prev;
+        Node next;
+
+        public Node(Integer value){
+            this.value = value;
+            this.prev = null;
+            this.next = null;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value, prev, next);
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    // o(n) time | o(1) space
+    public boolean containsNodeWithValue (Integer value){
+        Node node = head;
+        while(node != null){
+            if(node.value == value)
+                return true;
+            node = node.next;
+        }
+        return false;
+    }
+    //Logic number 2
+    // o(n) time | o(1) space
+    public boolean containsNodeWithValue2 (Integer value){
+        Node node = head;
+        while(node != null && node.value != value){
+            node = node.next;
+        }
+
+        return node != null;
+    }
+
+    // o(1) time | o(1) space
+    public void removeNode(Node node){
+        if(node == null)
+            return;
+
+        if(node == head){
+            head = head.next;
+        }
+
+        if (node == tail){
+            tail = tail.prev;
+        }
+
+        //now removing bindings from given node
+        rmeoveBindings(node);
+    }
+
+    // o(1) time | o(1) space
+    private void rmeoveBindings(Node node) {
+        if(node.prev != null){
+            node.prev.next = node.next;
+        }
+
+        if(node.next != null){
+            node.next.prev = node.prev;
+        }
+
+        node.prev = null;
+        node.next = null;
+    }
+
+    // o(n) time | o(1) space
+    public void removeNodesWithValue (Integer value){
+        Node node = head;
+        while(node != null){
+            Node nodeToRemove = node;
+            node = node.next;
+            if(nodeToRemove.value == value){
+                removeNode(nodeToRemove);
+            }
+        }
+    }
+
+    // o(1) time | o(1) space
+    public void insertBefore(Node node, Node nodeToInsert){
+        //if theres only one node in the list, don't do anything
+        //remove nodeToInsert if it already exists
+
+        if(nodeToInsert == head && nodeToInsert == tail){
+            return;
+        }
+
+        //removing if node already exists
+        removeNode(nodeToInsert);
+
+        //start adding node before
+        nodeToInsert.prev = node.prev;
+        nodeToInsert.next = node;
+        if(node.prev == null) { //if we are inserting before head
+            head = nodeToInsert;
+        }else{
+            node.prev.next = nodeToInsert;
+        }
+        node.prev = nodeToInsert;
+
+    }
+
+    // o(1) time | o(1) space
+    public void insertAfter(Node node, Node nodeToInsert){
+        //if theres only one node in the list, don't do anything
+        //remove nodeToInsert if it already exists
+
+        if(nodeToInsert == head && nodeToInsert == tail){
+            return;
+        }
+
+        //removing if node already exists
+        removeNode(nodeToInsert);
+
+        //start adding node before
+        nodeToInsert.prev = node;
+        nodeToInsert.next = node.next;
+
+        if(node.next == null){
+            tail = nodeToInsert;
+        }else{
+            node.next.prev = nodeToInsert;
+        }
+        node.next = nodeToInsert;
+    }
+
+    // o(1) time | o(1) space
+    private void setHead(Node node) {
+        if(head == null){
+            head = node;
+            tail = node;
+            return;
+        }
+        insertBefore(head, node);
+    }
+
+    // o(1) time | o(1) space
+    private void setTail(Node node) {
+        if(tail == null){
+            setHead(node);
+            return;
+        }
+        insertAfter(tail, node);
+    }
+
+    // o(n) time | o(1) space
+    public void insertAtPostion(Integer position, Node nodeToInsert){
+        if(position == 1){ //at head position
+            setHead(nodeToInsert);
+            return;
+        }
+
+        Node node = head;
+        Integer currentPosition = 1; //Let's keep index as 1
+        while(node != null && currentPosition != position){
+            node = node.next;
+            currentPosition++;
+        }
+
+        if(node != null){
+            insertBefore(node, nodeToInsert);
+        }else{ //we're at tail
+            setTail(nodeToInsert);
+        }
+    }
+
+    public Node insertAtTheEnd(int value) {
+        Node newNode = new Node(value);
+        if(head == null){
+            setHead(newNode);
+            setTail(newNode);
+            return newNode;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+        return newNode; //just to get the node to test remove functionality
+    }
+
+
+    public void print() {
+        Node node = head;
+
+        while(node != null){
+            System.out.print(node.value + " --> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public void printReverse() {
+        Node node = tail;
+
+        while(node != null){
+            System.out.print(node.value + " --> ");
+            node = node.prev;
+        }
+        System.out.println();
+    }
+
+}
--- data-structures/src/main/java/linkedList/LinkedListRunner.java
+++ data-structures/src/main/java/linkedList/LinkedListRunner.java
@@ -3,17 +3,35 @@ package linkedList;
 public class LinkedListRunner {
 
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
+        DoublyLinkedList list = new DoublyLinkedList();
 
         list.insertAtTheEnd(1);
         list.insertAtTheEnd(2);
         list.insertAtTheEnd(3);
-        list.insertAtTheEnd(4);
+        DoublyLinkedList.Node nodeToInsert = list.insertAtTheEnd(4);
         list.insertAtTheEnd(5);
-        list.insertAtTheEnd(6);
+        DoublyLinkedList.Node node = list.insertAtTheEnd(6);
 
         list.print();
         list.printReverse();
 
+        boolean isPresent = list.containsNodeWithValue2(1);
+        System.out.println(isPresent);
+
+        //System.out.println("Removing node: " + node.value);
+        //list.removeNode(node);
+        //list.removeNodesWithValue(4);
+        //list.print();
+
+        //list.insertBefore(node, nodeToInsert);
+        //list.print();
+
+        //list.insertAfter(node, nodeToInsert);
+        //list.print();
+        //list.printReverse();
+
+        nodeToInsert.value = 10;
+        list.insertAtPostion(3, nodeToInsert);
+        list.print();
     }
 }
